67cd13ad50c42a69e11673e82a938482686c3f23
Patrick Perini <pcperini@gmail.com>
2015-09-11 23:37:54 -0700

    limit to 10 stories

--- Grasshopper/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Grasshopper/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -57,6 +57,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/Bolts.framework'
   install_framework 'Pods/Parse.framework'
   install_framework 'Pods/UICollectionViewLeftAlignedLayout.framework'
+  install_framework 'Pods/UIScrollView_InfiniteScroll.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/ACBInfoPanel.framework'
@@ -64,4 +65,5 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/Bolts.framework'
   install_framework 'Pods/Parse.framework'
   install_framework 'Pods/UICollectionViewLeftAlignedLayout.framework'
+  install_framework 'Pods/UIScrollView_InfiniteScroll.framework'
 fi
